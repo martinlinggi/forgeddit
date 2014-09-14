44f7d14bb6123497e2a1c6765d36b9cfdd0a6a2f
martinlinggi <ma-li@hispeed.ch>
2014-09-14 21:30:51 +0200

    Refactoring: - REST-API erweitert: Voting is möglich (nur einmal pro Link) und Kommentare können eingegeben werden. - Views strukturiert (mit Partials)

--- app/scripts/forgeddit.js
+++ app/scripts/forgeddit.js
@@ -6,11 +6,62 @@ var app = angular.module('forgedditApp', []);
 
 var linkList;
 
-app.controller('LinkController', ['$scope', '$http', function($scope, $http) {
+/**
+ * Creates a text in the form "<time>-ago" from the given creation time
+ * @param creationTime The creation time
+ * @returns {string}
+ */
+function getTimeAgo(time)
+{
+    var millis = new Date().getTime() - time;
+    var oneSec = 1000;
+    var oneMin = oneSec*60;
+    var oneHour = oneMin*60;
+    var oneDay = oneHour*24;
+    var oneWeek = oneDay*7;
+    var oneMonth = oneDay*31;
+    var oneYear = oneDay*365;
+    if (millis < oneSec) {
+        return 'just now';
+    }
+    else if (millis < 10 * oneSec) {  // 1 second steps
+        return Math.round(millis / oneSec) + ' seconds ago';
+    }
+    else if (millis < oneMin) {  //  10 seconds steps
+        return Math.round(millis / (oneSec * 10) ) * 10 + ' seconds ago';
+    }
+    else if (millis < oneMin * 10) {  // 1 minute steps
+        return Math.round(millis / oneMin) + ' minutes ago';
+    }
+    else if (millis < oneHour) {  // 10 minute steps
+        return Math.round(millis / (10 * oneMin)) * 10 + ' minutes ago';
+    }
+    else if (millis < oneDay) {  // 1 hour steps
+        return Math.round(millis / oneHour) + ' hours ago';
+    }
+    else if (millis < oneWeek) {  //  1 day steps
+        return Math.round(millis / oneDay) + ' days ago';
+    }
+    else if (millis < oneMonth) {  //  1 week steps
+        return Math.round(millis / oneWeek) + ' weeks ago';
+    }
+    else if (millis < oneYear) {  //  1 month steps
+        return Math.round(millis / oneMonth) + ' months ago';
+    }
+    else {  // 1 year steps
+        return Math.round(millis / oneYear) + ' years ago';
+    }
+}
+
+app.controller('linkController', ['$scope', '$http', function($scope, $http) {
 
     $scope.showCommentsIndex = -1;
     $scope.predicate = 'time';
     $scope.reverse = true;
+
+    $scope.getTimeAgo = function(time) {
+        return getTimeAgo(time);
+    };
 
     // Gets all the links from the api
     $http.get('api').
@@ -21,7 +72,7 @@ app.controller('LinkController', ['$scope', '$http', function($scope, $http) {
         }).
         error(function(data, status, header, config){
             console.log('error: ' + status);
-
+// TODO Fix Error when send link does not work
         });
 
     // Sort the link list
@@ -31,24 +82,15 @@ app.controller('LinkController', ['$scope', '$http', function($scope, $http) {
         console.log('sort: ' + predicate + ' ' + reverse);
     };
 
-
-    $scope.setShowCommentsIndex = function (index) {
-        if (this.links[index].comments.length === 0)
-        {
-            return;
-        }
-        else if ($scope.showCommentsIndex === index) {
-            $scope.showCommentsIndex = -1;
-        }
-        else {
-            $scope.showCommentsIndex = index;
-        }
-    };
-
-    $scope.isShowComments = function (index) {
-        return $scope.showCommentsIndex === index;
-    };
 }]);
+
+app.directive('linkView', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/link-view.html'
+    };
+});
+
 
 app.controller('addLinkController', ['$scope', '$http', function($scope, $http) {
     $scope.title = "";
@@ -73,4 +115,88 @@ app.controller('addLinkController', ['$scope', '$http', function($scope, $http) 
 
 }]);
 
+app.directive('sendLinkView', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/send-link-view.html'
+    };
+});
 
+app.controller('linkContentController', ['$scope', '$http', function($scope, $http) {
+
+    $scope.newComment = "";
+    $scope.toggleComments = function(link)
+    {
+        $scope.showComments = !$scope.showComments;
+    };
+
+    $scope.sendComment = function(link)
+    {
+        console.log('Send Comment:');
+        var dataObject = {
+            text: $scope.newComment,
+            user: 'testuser',
+            time: new Date().getTime()
+        };
+        $http.post('/api/links/' + link._id + "/comments", dataObject)
+            .success(function (data, status, headers, config) {
+                link.comments.push(dataObject);
+                console.log('Success: Send Comment');
+                $scope.newComment = "";
+            })
+            .error(function (data, status, headers, config) {
+                console.log('Error: send Comment');
+// TODO Fix Error when send link does not work
+            });
+    }
+
+}]);
+
+app.controller('linkVoteController', ['$scope', '$http', function($scope, $http) {
+    $scope.hasAlreadyVoted = false;
+    $scope.vote = function(link, vote) {
+
+        if ($scope.hasAlreadyVoted === false)
+        {
+            console.log("Can Vote");
+            var dataObject = {
+                vote: vote
+            };
+            $http.put('/api/links/' + link._id, dataObject)
+                .success(function (data, status, headers, config) {
+                    link.rate += vote;
+                    $scope.hasAlreadyVoted = true;
+                    console.log('Success: Voting accepted');
+                })
+                .error(function (data, status, headers, config) {
+                    console.log('Error: Voting failed');
+// TODO Fix Error when send link does not work
+                });
+        }
+        else
+        {
+            console.log("Can't Vote");
+        }
+    };
+}]);
+
+app.directive('linkVoteView', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/link-vote-view.html'
+    };
+});
+
+app.directive('linkThumbView', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/link-thumb-view.html'
+    };
+});
+
+app.directive('linkContentView', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/link-content-view.html'
+    };
+});
--- routes/api.js
+++ routes/api.js
@@ -1,51 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var data =
-    [{  "id" : 1,
-        "title" : "Sony streamt Spiele auf Fernseher",
-        "url" : "http://www.heise.de/newsticker/meldung/E3-Sony-streamt-Spiele-auf-Fernseher-zeigt-neue-PS4-Titel-2218073.html",
-        "user" : "MaLiMaster",
-        "group" : "fun",
-        "rate" : 432,
-        "time" : 1308774240669,
-        "comments": [
-            {"user" : "tinMan", "time": 1308774240669, "text": "Hallo"},
-            {"user" : "MaLiMaster", "time": 1308774240669, "text": "Comment"}]
-    },
-        {   "id" : 2,
-            "title" : "Kurios gesammelt",
-            "url" : "http://www.heise.de/newsticker/meldung/Messesplitter-Kurioses-und-Bemerkenswertes-von-der-Computex-2217665.html",
-            "user" : "MaLiMaster",
-            "group" : "fun",
-            "rate" : 15,
-            "time" : 1308124245826,
-            "comments": []
-        },
-        {  "id" : 3,
-            "title" : "Möchtegern Hausbauer",
-            "url" : "http://i.imgur.com/RJqRpbM.jpg",
-            "user" : "MaLiMaster",
-            "group" : "fun",
-            "rate" : 22,
-            "time" : 1308124245826,
-            "comments": [
-                {"user" : "tinMan", "time": 1308774240669, "text": "Hallo"}]
-        },
-        {   "id" : 4,
-            "title" : "Giraffes in love",
-            "url" : "http://www.freemake.com/blog/wp-content/uploads/2013/07/animated-gifs-giraffes-51.gif",
-            "user" : "MaLiMaster",
-            "group" : "fun",
-            "rate" : 578,
-            "time" : 1308124245826,
-            "comments": []
-        }];
-
+var linkStore = require('../models/linklist.js');
 
 /* GET all the links. */
 router.get('/', function(req, res) {
-  res.json(data);
+    linkStore.getAllLinks(function (err, docs) {
+        res.json(docs);
+    });
 });
 
 // create a link
@@ -62,10 +24,39 @@ router.route('/links').post(function(req, res) {
     linkData.time = time;
     linkData.comments = [];
 
-    data.push(linkData);
+    linkStore.addLink(linkData, function(err, doc) {
+        res.json(doc);
+    });
 
-    res.json(linkData);
 });
+
+// votes a link
+router.route('/links/:linkId')
+    .get(function(req, res) {
+
+    })
+    .put(function(req, res) {
+        console.log('voting received: ' + req.params.linkId);
+        var vote = req.body.vote;
+        linkStore.voteLink(req.params.linkId, vote, function(err, numReplaced) {
+            res.json(numReplaced);
+        });
+
+    });
+
+// votes a link
+router.route('/links/:linkId/comments')
+    .post(function(req, res) {
+        console.log('comment received: ' + req.params.linkId);
+        var comment = {};
+        comment.text = req.body.text;
+        comment.user = req.body.user;
+        comment.time = new Date().getTime();
+        linkStore.addComment(req.params.linkId, comment, function(err, numReplaced) {
+            res.json(numReplaced);
+        });
+
+    });
 
 
 module.exports = router;
