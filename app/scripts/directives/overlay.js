b8ae1e57231b81445ee7c21e27d6475b722dbb39
mwirrer <Administrator@dsp2013.domserver12.com>
2014-11-24 22:10:17 +0100

    Overlay directive for modal popup overlay

---
+++ app/scripts/directives/overlay.js
@@ -0,0 +1,23 @@
+//app.directive('modalDialog', function() {
+  angular.module('forgedditApp').directive('modalDialog', function () {
+  console.log('Overlay directive called');
+  return {
+    restrict: 'E',
+    scope: {
+      show: '='
+    },
+    replace: true, // Replace with the template below
+    transclude: true, // we want to insert custom content inside the directive
+    link: function(scope, element, attrs) {
+      scope.dialogStyle = {};
+      if (attrs.width)
+        scope.dialogStyle.width = attrs.width;
+      if (attrs.height)
+        scope.dialogStyle.height = attrs.height;
+      scope.hideModal = function() {
+        scope.show = false;
+      };
+    },
+    templateUrl: 'templates/modal_dialog.html'
+  };
+});
