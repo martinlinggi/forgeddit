fb56e94ab807ec82de12530dd43e60a39e056434
martinlinggi <ma-li@hispeed.ch>
2014-11-25 22:53:12 +0100

    new route: getLink/:id, authentication refactoring using secret.js, socket-io for vote-updates

--- routes/api.js
+++ routes/api.js
@@ -3,18 +3,27 @@
     var express = require('express');
     var router = express.Router();
 
+    var secret = require('./secret.js');
     var ForgedditStore = require('../models/link_store.js');
     var VoteStore = require('../models/vote_store.js');
 
     /* GET all the links. */
     router.get('/links', function (req, res) {
-        ForgedditStore.getAllLinks(function (err, docs) {
-            res.json(docs);
+        ForgedditStore.getAllLinks(function (err, links) {
+            res.json(links);
+        });
+    });
+
+    /* GET one link. */
+    router.get('/links/:linkId', function (req, res) {
+        var linkId = req.params.linkId;
+        ForgedditStore.getLink(linkId, function (err, link) {
+            res.json(link);
         });
     });
 
 // create a link
-    router.post('/links', function (req, res) {
+    router.post('/links', secret.secured(), function (req, res) {
         var time = new Date().getTime();
         var linkData = {};
         linkData.user = req.body.user;
@@ -32,20 +41,19 @@
     });
 
 // votes a link
-    router.put('/links/:linkId/vote/', function (req, res) {
+    router.put('/links/:linkId/vote/', secret.secured(), function (req, res) {
         var vote = req.body.value;
         var userName = req.body.userName;
         var linkId = req.params.linkId;
         ForgedditStore.voteLink(linkId, vote, function (err, numReplaced) {
             VoteStore.doVote(linkId, userName, vote);
             res.json(numReplaced);
-
+            req.io.route('updateLink', linkId);
         });
     });
 
 // add a comment
-    router.route('/links/:linkId/comments')
-        .post(function (req, res) {
+    router.post('/links/:linkId/comments', secret.secured(), function (req, res) {
             console.log('comment received: ' + req.params.linkId);
             var comment = {};
             comment.text = req.body.text;
