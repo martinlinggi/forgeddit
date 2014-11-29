f9be07653c526cae43189fdd1793a78781d172b4
martinlinggi <ma-li@hispeed.ch>
2014-11-29 03:28:16 +0100

    Some styling for Desktop

--- app/scripts/directives/link_thumb.js
+++ app/scripts/directives/link_thumb.js
@@ -1,3 +1,23 @@
 /**
- * Created by martinlinggi on 28.11.14.
+ * @brief Angular-directive for the thumbs-image template
+ *
+ * @file link_thumb.js
+ * @author martin linggi
  */
+
+(function() {
+    'use strict';
+
+    angular.module('forgedditApp').directive('linkThumbView', function () {
+
+        //=====================================================================
+        // Directive API
+        //=====================================================================
+
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/link_thumb_view.html'
+        };
+    });
+
+}());
